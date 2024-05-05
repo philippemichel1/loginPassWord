353590498254d33dba09556f09c519f5ded6f45d
Philippe MICHEL <philippe.michel1@gmail.com>
2024-05-05 17:05:28 +0200

    Initial Commit

---
+++ LoginPassWord/LoginPassWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPassWordApp.swift
+//  LoginPassWord
+//
+//  Created by Philippe MICHEL on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginPassWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
