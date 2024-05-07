06324ebf356224672866610831b5ad663ac81a99
Philippe MICHEL <philippe.michel1@gmail.com>
2024-05-07 15:38:31 +0200

    correction

--- LoginPassWord/View/ContentView.swift
+++ LoginPassWord/View/ContentView.swift
@@ -12,16 +12,32 @@ struct ContentView: View {
     @State private var password: String = ""
     @State private var isAuthenticated: Bool = false
     @State private var userChoise:Int = 0
+    @State private var showAlert:Bool = false
 
-    
+
     var body: some View {
         NavigationStack {
+            ZStack{
+                // fond
+                Color.myGreen
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white).opacity(0.16)
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
             VStack {
+                Text("Login")
+                    .font(.largeTitle)
+                    .bold()
                 Image((listOfUsers.listUsers()[userChoise].userImage))
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width:150, height: 150)
                     .clipShape(Circle())
+                    .shadow(color: .gray, radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                    
                     .padding()
                     
                 // picker de selection des utilisateurs
@@ -29,13 +45,11 @@ struct ContentView: View {
                     ForEach(0..<listOfUsers.listUsers().count, id: \.self) { index in
                         Text("Utilisateur \(listOfUsers.listUsers()[index].userName)").tag(index)
                     }
-                
                 }
                 .pickerStyle(.automatic)
                 .onChange(of: userChoise) {
                  password = ""
                 isAuthenticated = false
-                
                 }
             
                 SecureField("Mot de passe", text: $password)
@@ -52,49 +66,51 @@ struct ContentView: View {
                 .foregroundColor(.white)
                 .background(isAuthenticated ? Color.green : Color.blue)
                 .cornerRadius(5)
+                // Champ est à vide
+                .onAppear {
+                    password = ""
+                }
                 
-                
-
-                
-                loginStatusMessage
+                .navigationDestination(isPresented: $isAuthenticated) {
+                    ZStack {
+                        Color.yellow
+                            .ignoresSafeArea()
+                        Circle()
+                            .scale(1.7)
+                            .foregroundColor(.white).opacity(0.16)
+                        Circle()
+                            .scale(1.35)
+                            .foregroundColor(.white)
+                        VStack {
+                            Text("Vous etes logger sous: \(listOfUsers.listUsers()[userChoise].userName)").tag(userChoise)
+                        }
+                    }
+                }
+                .alert("Erreur", isPresented: $showAlert) {
+                    //loginStatusMessage
+                    Text("Erreur de connexion")
+                    Button("OK", role: .cancel) { }
+                    
+                }
             }
             .padding()
-        }.navigationBarHidden(true)
+            }
+        }
+        .navigationBarHidden(true)
+        
+        
         
             
     }
     
-       
 //authentification utilisateur
     func authenticateUser(password: String) {
         if  password == listOfUsers.listUsers()[userChoise].passWord {
             isAuthenticated = true
         } else {
             isAuthenticated = false
+            showAlert.toggle()
         }
-    }
-    
-
-    var loginStatusMessage: some View {
-        Text(isAuthenticated ? "Connecté avec succès!" : "Échec de la connexion")
-            .foregroundColor(isAuthenticated ? .green : .red)
-    
-            .navigationDestination(isPresented: $isAuthenticated) {
-                ZStack {
-                    Color.yellow
-                        .ignoresSafeArea()
-                    Circle()
-                        .scale(1.7)
-                        .foregroundColor(.white).opacity(0.16)
-                    Circle()
-                        .scale(1.35)
-                        .foregroundColor(.white)
-                    VStack {
-                        Text("Vous etes logger sous: \(listOfUsers.listUsers()[userChoise].userName)").tag(userChoise)
-                    }
-                }
-            }
-        
     }
 }
 
