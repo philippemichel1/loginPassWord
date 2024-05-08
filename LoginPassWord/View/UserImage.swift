c6c5c7737f0fe835f72bb87d460e771a92863134
Philippe MICHEL <philippe.michel1@gmail.com>
2024-05-08 11:40:16 +0200

    Creation vue image + vue picker

---
+++ LoginPassWord/View/UserImage.swift
@@ -0,0 +1,18 @@
+//
+//  UserImage.swift
+//  LoginPassWord
+//
+//  Created by Philippe MICHEL on 08/05/2024.
+//
+
+import SwiftUI
+
+struct UserImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserImage()
+}
