b5130491f3432f856bffa6cee81f04b2f23d3c3b
Philippe MICHEL <philippe.michel1@gmail.com>
2024-05-08 13:10:42 +0200

    Création de ma vue personne connectée

---
+++ LoginPassWord/View/UserConnectedView.swift
@@ -0,0 +1,18 @@
+//
+//  UserConnectedView.swift
+//  LoginPassWord
+//
+//  Created by Philippe MICHEL on 08/05/2024.
+//
+
+import SwiftUI
+
+struct UserConnectedView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserConnectedView()
+}
