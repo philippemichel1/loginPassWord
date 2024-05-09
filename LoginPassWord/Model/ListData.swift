c5521c3e7503fbfbc459dbe04dfa96a33d80850a
Philippe MICHEL <philippe.michel1@gmail.com>
2024-05-09 13:17:42 +0200

    correction utilisateur

--- LoginPassWord/Model/ListData.swift
+++ LoginPassWord/Model/ListData.swift
@@ -9,7 +9,7 @@ import Foundation
 import SwiftUI
 class ListData {
     func listUsers() -> [User] {
-        return [User(userName: "titastus.com", userImage: "titastus.com", passWord: "1969"),
+        return [User(userName: "titastus.com", userImage: "titastus", passWord: "1969"),
                  User(userName: "philippe", userImage: "men", passWord: "1234"),
                  User(userName: "Sandrine", userImage: "wooman", passWord: "5678")]
     }
