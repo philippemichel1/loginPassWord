5df63036daf0c89f6c840c167213867fe9e686bf
Philippe MICHEL <philippe.michel1@gmail.com>
2024-05-09 12:23:21 +0200

    creation de la vue titre

---
+++ LoginPassWord/View/InterfaceDesignView.swift
@@ -0,0 +1,18 @@
+//
+//  InterfaceDesignView.swift
+//  LoginPassWord
+//
+//  Created by Philippe MICHEL on 09/05/2024.
+//
+
+import SwiftUI
+
+struct InterfaceDesignView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InterfaceDesignView()
+}
